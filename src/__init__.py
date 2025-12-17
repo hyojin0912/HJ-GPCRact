6fd61e91c2509b8eec105bdb56e85da11d5c42ed
Sonhyojin <39079639+hyojin0912@users.noreply.github.com>
2025-12-17 16:03:20 +0900

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
-from .model import DAGN_HybridModel
+from .model import GPCRact_Model
 from .dataset import GraphDataset, collate_fn, get_valid_indices
 from .utils import set_seed, EarlyStopping
+
 from .loss import FocalLoss
