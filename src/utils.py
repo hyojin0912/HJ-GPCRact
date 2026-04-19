0bec5c6539a9def2747a53f36f417c8c36984739
Hyojin Son <39079639+hyojin0912@users.noreply.github.com>
2026-04-20 00:02:09 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,8 @@ def set_seed(seed):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 class EarlyStopping:
     """Early stops the training if validation score doesn't improve after a given patience."""
